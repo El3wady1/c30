6289b1e1f711adaca7c8a306b33a842f6d11dacc
Mahmoud Elawady <125752461+El3wady1@users.noreply.github.com>
2025-09-25 08:02:53 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,27 +5,42 @@ const urlsToCache = [
   '/styles/main.css',
   '/script/main.js',
   '/images/logo.png',
-  '/manifest.json'
+  '/manifest.json',
+  '/offline.html'
 ];
 
-self.addEventListener('install', function(event) {
+// Install
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-self.addEventListener('fetch', function(event) {
+// Activate
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
+    caches.match(event.request).then(response => {
+      return (
+        response ||
+        fetch(event.request).catch(() => caches.match('/offline.html'))
+      );
+    })
   );
 });
